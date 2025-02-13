3255294491629cfd3ab5c838939ef32f43dd907b
P1kus <[piotrpopiolekk@gmail.com]>
2025-02-13 20:55:48 +0100

    Added about section v1

--- app/components/about/about.tsx
+++ app/components/about/about.tsx
@@ -1,16 +1,27 @@
 import styles from "./about.module.css"
 import map from "../../assets/map.png"
+import { HorizontalLine } from "../horizontalLine/horizontalLine"
 
 export function About() {
   return (
     <div className={styles.container}>
       <img src={map} alt="Map of Poland, with Lublin pinpointed on the map" />
       <ul>
-        <h3>Check me out on</h3>
+        <h3>Profiles</h3>
+        <h4>Dev</h4>
         <li>Github</li>
+        <li>Codepen</li>
         <li>Linkedin</li>
-        <li></li>
-        <li></li>
+
+        {/* https://github.com/p1kus */}
+        {/* https://codepen.io/p1kus */}
+        <h4>Creative</h4>
+        <li>Instagram</li>
+        <li>Youtube</li>
+        <h4>Certificates</h4>
+        <li>Cisco Networking Basics</li>
+        <li>Cisco Introduction to IoT</li>
+        <li>Cisco Introduction to Cybersecurity</li>
       </ul>
     </div>
   )
