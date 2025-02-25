fdb5745d0d20fe6bf98cfbbd4e08c09293390dae
P1kus <[piotrpopiolekk@gmail.com]>
2025-02-25 22:10:07 +0100

    Preload for quicker start

--- app/root.tsx
+++ app/root.tsx
@@ -29,6 +29,11 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Faustina:ital,wght@0,300..800;1,300..800&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap",
   },
+  {
+    rel: "preload",
+    href: "/.netlify/entry.client-Crbb1COA.js",
+    as: "script",
+  },
 ];
 
 export function Layout() {
